331fa01808d3e3f76f89f022b58046ab53821274
Patrick James Mardis <59069682+patrickjamesmardis@users.noreply.github.com>
2021-01-30 14:11:57 -0500

    rects for grid

--- js/main.js
+++ js/main.js
@@ -3,11 +3,11 @@ let delay = 0;
 let position = 0;
 tl.set("circle", { opacity: 0 });
 
-// tl.set(".row5.col6", {
+// tl.set(".rows .row5.col5", {
 //     r: 10
-// }).to(".row5.col6", {
+// }).to(".rows .row5.col5", {
 //     opacity: 1
-// }).to(".row5.col6", {
+// }).to(".rows .row5.col5", {
 //     r: 100,
 //     repeat: 5,
 //     yoyo: true,
@@ -15,72 +15,75 @@ tl.set("circle", { opacity: 0 });
 //     delay: .3,
 //     repeatDelay: .3,
 //     ease: "circ.inout"
-// }).to(".row5.col6", {
+// }).to(".rows .row5.col5", {
 //     r: 1,
 //     duration: .16,
 //     opacity: 0
-// }).to(".row5", {
+// }).to(".rows .row5", {
 //     opacity: 1,
 //     stagger: 0.25
 // });
 
-tl.set(".row4, .row6", {
+tl.set(".rows .row4, .rows .row6", {
     cy: "50%"
-}).to(".row4, .row6", {
+}).to(".rows .row4, .rows .row6", {
     cy: (el) => { // passing el in gives an index of the element. el 0 - 10 are row 4, 11 - 21 are row 6
         return el < 11 ? "40%" : "60%";
     },
     opacity: 1
-}).set(".row3, .row7", { // el 0 - 10 are row 3, 11 - 21 are row 7
+}).set(".rows .row3, .rows .row7", { // el 0 - 10 are row 3, 11 - 21 are row 7
     cy: (el) => {
         return el < 11 ? "40%" : "60%";
     }
-}).to(".row3, .row7", {
+}).to(".rows .row3, .rows .row7", {
     cy: (el) => {
         return el < 11 ? "30%" : "70%";
     },
     opacity: 1,
 
-}).set(".row2, .row8", {// el 0 - 10 are row 2, 11 - 21 are row 8
+}).set(".rows .row2, .rows .row8", {// el 0 - 10 are row 2, 11 - 21 are row 8
     cy: (el) => {
         return el < 11 ? "30%" : "70%";
     }
-}).to(".row2, .row8", {
+}).to(".rows .row2, .rows .row8", {
     cy: (el) => {
         return el < 11 ? "20%" : "80%";
     },
     opacity: 1
-}).set(".row1, .row9", {// el 0 - 10 are row 1, 11 - 21 are row 9
+}).set(".rows .row1, .rows .row9", {// el 0 - 10 are row 1, 11 - 21 are row 9
     cy: (el) => {
         return el < 11 ? "20%" : "80%";
     }
-}).to(".row1, .row9", {
+}).to(".rows .row1, .rows .row9", {
     cy: (el) => {
         return el < 11 ? "10%" : "90%";
     },
     opacity: 1
-}).set(".row0, .row10", {// el 0 - 10 are row 0, 11 - 21 are row 10
+}).set(".rows .row0, .rows .row10", {// el 0 - 10 are row 0, 11 - 21 are row 10
     cy: (el) => {
         return el < 11 ? "10%" : "90%";
     }
-}).to(".row0, .row10", {
+}).to(".rows .row0, .rows .row10", {
     cy: (el) => {
         return el < 11 ? "0%" : "100%";
     },
     opacity: 1
-}).to("circle", {
-    cy: 0
-}).to("circle", {
+}).to(".rows circle", {
+    cy: 0,
+    duration: 1.5
+}).to(".rows circle", {
     cy: "100%",
+    duration: 1.5,
     delay: (el) => { // 11 circles makes a row
         delay = (el % 11 == 0) ? delay + .01 : delay;
         return delay;
     }
-}).to("circle", {
+}).to(".rows circle", {
     cy: (el) => {
         position = (el % 11 == 0) ? position + 10 : position;
         return position + "%";
-    }
+    },
+    duration: 1.5
 });
 
 delay = 0;
@@ -88,7 +91,47 @@ position = 0;
 
 tl.to("circle", {
     cx: 0
-}).to("circle", {
+}).set(".cols circle", {
+    opacity: 1
+}).set(".rows circle", {
+    opacity: 0
+}).to(".cols circle", {
     cx: "100%",
-    stagger: 0.1
-})
+    duration: 1.5,
+    delay: (el) => {
+        delay = (el % 11 == 0) ? delay + .01 : delay;
+        return delay;
+    }
+}).to(".cols circle", {
+    cx: (el) => {
+        position = (el % 11 == 0) ? position - 10 : position;
+        return position + "%";
+    },
+    duration: 1.5
+});
+
+
+for (let i = 0; i < 30; i++) {
+    let rand1 = Math.floor(Math.random() * 10);
+    let rand2 = Math.floor(Math.random() * 10);
+    tl.to(`.cols .row${rand1}.col${rand2}`, {
+        stroke: "#FF5A35",
+        duration: 0.1
+    });
+}
+tl.to(".cols circle", {
+    stroke: "#dfdfdf"
+});
+tl.set("rect", {
+    width: 1,
+    height: 1
+}).to(".vertical, .horizontal", {
+    height: (el) => {
+        return el < 11 ? "100%" : 1;
+    },
+    width: (el) => {
+        return el < 11 ? 1 : "100%";
+    }
+}).to(".cols circle", {
+    opacity: 0
+});
